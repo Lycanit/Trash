bf1f581b1437a0b95e2f4b6885575b684ff8a7b6
Lycan <kroitoru.g.g@gmail.com>
2016-09-14 21:18:39 +0300

    fuck up

--- ConsoleApplication/ConsoleApplication/Dijcstra/Dijcstra.cs
+++ ConsoleApplication/ConsoleApplication/Dijcstra/Dijcstra.cs
@@ -9,7 +9,7 @@ namespace ConsoleApplication.Dijcstra
     class Dijcstra
     {
 
-
+        private static int _shift = 2;
 
         public static Node generate()
         {
@@ -25,8 +25,11 @@ namespace ConsoleApplication.Dijcstra
             n1.AddNode(n4, 40);
             n1.AddNode(n5, 50);
 
-            n3.AddNode(n5, 10);
+            n3.AddNode(n4, 25);
+            n3.AddNode(n5, 15);
 
+            n5.AddNode(n4, 25);
+           
 
             return n1;
         }
@@ -40,18 +43,58 @@ namespace ConsoleApplication.Dijcstra
 
         public static void print(Node node)
         {
-            List<Node> list = new List<Node>();
-            list.Add(node);
+            Dictionary<int, List<int>> dic = new Dictionary<int, List<int>>();
+            List<PrintNode> list = new List<PrintNode>();
+            PrintNode _pn = new PrintNode()
+            {
+                Shift = 0,
+                FromNode = 0,
+                Node = node,
+            };
+            list.Add(_pn);
             while (list.Count > 0)
             {
-                Node n = list[0];
-                Console.WriteLine("points {0}", n.Data);
+                PrintNode pn = list[0];
+                var shiftStr = GetShift(pn.Shift);
+                Console.WriteLine($"{shiftStr}{pn.FromNode} -> {pn.Node.Data}");
+
                 list.RemoveAt(0);
-                for(int i = 0; i < n.List.Count; i++)
+                for(int i = 0; i < pn.Node.List.Count; i++)
                 {
-                    list.Add(n.List[i].Node);
+                    if (dic.ContainsKey(pn.Node.Data))
+                    {
+                        if (dic[pn.Node.Data].Contains(pn.Node.List[i].Node.Data))
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            dic[pn.Node.Data].Add(pn.Node.List[i].Node.Data);
+                        }
+                    }
+                    else
+                    {
+                        dic[pn.Node.Data] = new List<int>() { pn.Node.List[i].Node.Data };
+                    }
+                    PrintNode childPN = new PrintNode()
+                    {
+                        FromNode = pn.Node.Data,
+                        Shift = pn.Shift + 1,
+                        Node = pn.Node.List[i].Node,
+                    };
+                    list.Insert(0, childPN);
                 }
             }
+        }
+
+        public static string GetShift(int shift)
+        {
+            string ret = "";
+            for(int i = 0; i < shift * _shift; i++)
+            {
+                ret += " ";
+            }
+            return ret;
         }
 
         public static void ResolveSimple(Node start, Node end)
---
+++ ConsoleApplication/ConsoleApplication/Dijcstra/PrintNode.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication.Dijcstra
+{
+    class PrintNode
+    {
+        public int Shift;
+        public int FromNode;
+        public Node Node;
+
+        
+    }
+}
